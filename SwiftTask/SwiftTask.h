7cd27388cb610344ba827354bbc92bc952819863
Yasuhiro Inami <inamiy@gmail.com>
2014-09-01 21:19:41 +0900

    Add Xcodeproj-target for iOS-framework.

--- SwiftTask/SwiftTask.h
+++ SwiftTask/SwiftTask.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014年 Yasuhiro Inami. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftTask.
 FOUNDATION_EXPORT double SwiftTaskVersionNumber;
